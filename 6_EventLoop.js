7c15e676b97b84297d93d4d991eaf1a322eb189f
WXG9722 <1321844625@qq.com>
2019-03-27 19:58:52 +0800

    review

--- 6_EventLoop.js
+++ 6_EventLoop.js
@@ -1,8 +1,10 @@
-// 宏任务（console.log Promise等）
-// process.nextTick（他不是微任务 是宏微之间的 下一次事件循环之前执行的函数）
-// Promise.then(微任务)
+// https://nodejs.org/zh-cn/docs/guides/event-loop-timers-and-nexttick/
+// http://www.ruanyifeng.com/blog/2014/10/event-loop.html
+// 1.宏任务（script setTimeout setInterval setImmediate）
+// 2.process.nextTick（宏任务微任务的概念官方似乎没有提及 他不是微任务 是宏微之间的 下一次事件循环之前执行的函数）
+// 3.微任务（Promise整体是一个是微任务 只不过new Promise语句会立即执行类似于console.log 结果就是好像Promise是宏任务 Promise.then是微任务一样）
 // (Promise整体是个微任务 只不过new Promise语句让不在then里面的东西直接执行了 所以可以勉强理解为Promise是宏任务 Promise.then是微任务)
-// 异步任务（setTimeout setInterval setImmediate）
+// 浏览器中没有process.nextTick和setImmediate
 
 console.log('start');
 Promise.resolve().then(function() {
